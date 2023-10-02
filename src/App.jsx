b6bbcb8a81004978c697aae2b2a133063955dcb9
dnstrk <stfuidkama@gmail.com>
2023-10-02 23:11:25 +0300

    add fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import axios from "axios";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+    let [users, setUsers] = useState([]);
+
+    async function fetchUsers() {
+        const response = await axios.get(
+            "https://jsonplaceholder.typicode.com/users"
+        );
+        // console.log(response)
+        setUsers(response.data);
+        console.log(users)
+    }
+
+    // function show() {
+    //     // console.log(users)
+
+    //     return (
+            
+    //     );
+    // }
+
     return (
         <div className="App">
             <div>Test pages</div>
+            <button style={{'width': '100px'}} onClick={fetchUsers}>fetchUsers</button>
+            <div style={{'maxWidth': '600px', 'display': 'flex', 'flexDirection': 'column', 'alignItems': "flex-start"}}>
+                {users.map((user) => (
+                    <div key={user.id}>{user.id}. {user.name}</div>
+                ))}
+            </div>
+            {/* <button onClick={show}></button> */}
         </div>
     );
 }
---
+++ src/components/UsersList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function UsersList(users) {
+  return (
+    <div></div>
+  )
+}
