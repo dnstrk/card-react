b2485fea48ed3e7caeada6f77235638b97ffbb22
dnstrk <stfuidkama@gmail.com>
2023-10-02 16:56:46 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
+            <div style={{ "font-size": "24px" }}>Head</div>
             <div>Test pages</div>
         </div>
     );
