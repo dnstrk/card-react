038b284b52b178cc0187dcf2f279aba96b82a2e3
dnstrk <stfuidkama@gmail.com>
2023-10-03 16:17:41 +0300

    loading

---
+++ src/API/UserService.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import axios from "axios";
+
+export default class UserService extends Component {
+  static async getAll() {
+    try {
+        const response = await axios.get(
+            "https://jsonplaceholder.typicode.com/users"
+        );
+        return response.data
+    } catch (e) {
+        console.log(e)
+    }
+
+    
+  }
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,41 @@
-import axios from "axios";
 import "./App.css";
 import { useState } from "react";
-import UsersList from "./components/UsersList";
+import { useEffect } from "react";
+import UserService from "./API/UserService";
+import Loader from "./components/UI/Loader";
 
 function App() {
-    let [users, setUsers] = useState([]);
-    let [show, setShow] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [isUsersLoading, setIsUsersLoading] = useState(false);
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
 
     async function fetchUsers() {
-        const response = await axios.get(
-            "https://jsonplaceholder.typicode.com/users"
-        );
-        setUsers(response.data);
+        setIsUsersLoading(true);
+        setTimeout(async() => {
+            const users = await UserService.getAll();
+            setUsers(users);
+            setIsUsersLoading(false);
+        }, 1500)
+        
     }
 
     return (
         <div className="App">
-            <div>Test pages</div>
-            <button style={{ width: "100px" }} onClick={fetchUsers}>
-                fetchUsers
-            </button>
-            <div>
-            <button style={{ width: "100px" }} onClick={() => setShow(!show)}>
-                Show
-            </button>
-            <button style={{ width: "100px" }} onClick={() => setUsers([])}>
-                clear
-            </button>
-            </div>
-            <UsersList users={users} show={show} />
-            {/* <div
-                style={{
-                    maxWidth: "600px",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "flex-start",
-                }}
-            >
-                {users.map((user) => (
-                    <div key={user.id}>
-                        {user.id}. {user.name}
-                    </div>
-                ))}
-            </div> */}
-            {/* <div style={{'maxWidth': '600px', 'display': 'flex', 'flexDirection': 'column', 'alignItems': "flex-start"}}>
-                {users.map((user) => (
-                    <div key={user.id}>{user.id}. {user.name}</div>
-                ))}
-            </div> */}
-            {/* <button onClick={show}></button> */}
+            <div style={{ marginBottom: "20px" }}>Test pages</div>
+            {isUsersLoading ? (
+                <Loader />
+            ) : (
+                <div>
+                    {users.map((user) => (
+                        <div key={user.id}>
+                            {user.id}. {user.name}
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
