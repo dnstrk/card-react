c5edeeb99098340e1809c848df030319262e7593
dnstrk <stfuidkama@gmail.com>
2023-10-03 11:50:29 +0300

    show&clear btns

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,53 @@
 import axios from "axios";
 import "./App.css";
 import { useState } from "react";
+import UsersList from "./components/UsersList";
 
 function App() {
     let [users, setUsers] = useState([]);
+    let [show, setShow] = useState(false);
 
     async function fetchUsers() {
         const response = await axios.get(
             "https://jsonplaceholder.typicode.com/users"
         );
-        // console.log(response)
         setUsers(response.data);
-        console.log(users)
     }
-
-    // function show() {
-    //     // console.log(users)
-
-    //     return (
-            
-    //     );
-    // }
 
     return (
         <div className="App">
             <div>Test pages</div>
-            <button style={{'width': '100px'}} onClick={fetchUsers}>fetchUsers</button>
-            <div style={{'maxWidth': '600px', 'display': 'flex', 'flexDirection': 'column', 'alignItems': "flex-start"}}>
+            <button style={{ width: "100px" }} onClick={fetchUsers}>
+                fetchUsers
+            </button>
+            <div>
+            <button style={{ width: "100px" }} onClick={() => setShow(!show)}>
+                Show
+            </button>
+            <button style={{ width: "100px" }} onClick={() => setUsers([])}>
+                clear
+            </button>
+            </div>
+            <UsersList users={users} show={show} />
+            {/* <div
+                style={{
+                    maxWidth: "600px",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "flex-start",
+                }}
+            >
+                {users.map((user) => (
+                    <div key={user.id}>
+                        {user.id}. {user.name}
+                    </div>
+                ))}
+            </div> */}
+            {/* <div style={{'maxWidth': '600px', 'display': 'flex', 'flexDirection': 'column', 'alignItems': "flex-start"}}>
                 {users.map((user) => (
                     <div key={user.id}>{user.id}. {user.name}</div>
                 ))}
-            </div>
+            </div> */}
             {/* <button onClick={show}></button> */}
         </div>
     );
--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
 
-export default function UsersList(users) {
-  return (
-    <div></div>
-  )
+export default function UsersList({ users, show }) {
+    if (show) {
+        if (users.length > 0) {
+            return (
+                <div
+                    style={{
+                        maxWidth: "600px",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "flex-start",
+                    }}
+                >
+                    {users.map((user) => (
+                        <div key={user.id}>
+                            {user.id}. {user.name}
+                        </div>
+                    ))}
+                </div>
+            );
+        } else  {
+          return <div>Users list is empty</div>
+        }
+    } else {
+        return <div>Not found</div>;
+    }
 }
